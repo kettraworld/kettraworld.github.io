1d3aeaba15529b7b15d27c4dca3df8675ae194a9
sebastianjn007 <sebastianjn007@gmail.com>
2022-03-26 11:01:24 -0300

    novo template de site

--- README.md
+++
@@ -1,3 +0,0 @@
-# kettraworld.github.io
-
-em breve!
---
+++ src/js/scripts.js
@@ -0,0 +1,33 @@
+
+var ip = document.querySelector('#ip');
+var ipSpan = ip.querySelector('span');
+var ipTextarea = ip.querySelector('textarea');
+ip.addEventListener('click', function () {
+  ip.classList.add('is-active');
+  setTimeout(() => {
+    ip.classList.remove('is-active');
+  }, 1500);
+
+  ipTextarea.classList.add('is-active');
+  ipTextarea.value = ipSpan.innerHTML;
+  ipTextarea.select();
+  ipTextarea.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  ipTextarea.classList.remove('is-active');
+});
+
+$.getJSON('https://api.minetools.eu/ping/br-arm-7.enxadahost.com/10972' , function(data) {
+  if (data.error) {
+    $('#status').html('<i class="fas fa-times"></i> Servidor offline');
+    $('#motd').html('-');
+    $('#online').html('-');
+  } else {
+    $('#status').html('<i class="fas fa-check"></i> Servidor online');
+    $('#motd').html(data.description.replace(/§(.+?)/gi, ''));
+    $('#online').html(data.players.online);
+  }
+});
+
+$('.navbar-burger').on('click', function() {
+  document.querySelector('.navbar-menu').classList.toggle('is-active');
+});
