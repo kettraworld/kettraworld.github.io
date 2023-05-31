6ad6131b21b1f8669035461aa94c92ed2160836e
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2023-05-31 20:48:59 -0300

    update docs/src/js/cookie.js

--- docs/src/js/cookie.js
+++ docs/src/js/cookie.js
@@ -1,9 +1,16 @@
 const buttons = document.querySelectorAll('#cookie');
+const exit = document.querySelectorAll('#exit');
 
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
 		Cookies.set('POLICY', 'true', { expires: 7 });
 		$('.cookie').remove();
+	});
+});
+
+exit.forEach(button => {
+	button.addEventListener('click', () => {
+	  Cookies.remove('hash');
 	});
 });
 
