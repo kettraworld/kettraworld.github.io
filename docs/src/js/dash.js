0a3d095a5145d6e3a673d2645cebff528fa2abda
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-05-31 15:45:59 -0300

    dash update 1/5

--- docs/src/js/dash.js
+++ docs/src/js/dash.js
@@ -8,15 +8,11 @@ const hash = Base64.decode(Cookies.get('hash'));
 const [nick, password] = hash.split("/");
 
 $(document).ready(() => {
-$('#card-2').html(`
-  <img class="card-img" src="https://api.kettraworld.shop/player/skin/${nick}">
-    <div class="player-name">
-      <h1>${nick}</h1>
-     </div>
-     <div class="card-buttons">
-   <button class="card-button"><i class="fas fa-undo-alt"></i></button>
-   <button class="card-button"><i class="fas fa-info"></i></button>
-    <button class="card-button"><i class="fas fa-sign-in-alt"></i></button>
-   </div>
-`);
+  
+$.getJSON(`https://api.kettraworld.shop/player/skin/${nick}`, (data) => {
+ $('#player_skin').html(`<img class="card-img" src="https://mc-heads.net/head/${data.skin}">`);
+});
+
+$('#player_name').html(`<div class="player-name"><h1>${nick}</h1></div>`);
+
 });
