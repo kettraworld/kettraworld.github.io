240879c9532dc86aec954c4cd65b8c68cec9b8f1
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2023-06-22 12:12:02 -0300

    update docs/src/js/chat.js

--- docs/src/js/chat.js
+++ docs/src/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io("http://129.148.43.196:8888");
+const socket = io("http://129.148.43.196:8080");
 const input = document.getElementById('input');
 const send = document.getElementById('send');
 const audio = new Audio("https://cdn.discordapp.com/attachments/1077666564880486503/1121196909302915083/notify.mp3");
