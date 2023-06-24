9e291af25e6167c0b3fe682a88fbbd7921ad271c
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-06-24 19:49:06 -0300

    chat update 2/3

--- docs/src/js/chat.js
+++ docs/src/js/chat.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:8080");
+const socket = io("wss://socket.kettraworld.shop");
 const input = document.getElementById('input');
 const send = document.getElementById('send');
 const audio = new Audio("https://cdn.discordapp.com/attachments/1077666564880486503/1121196909302915083/notify.mp3");
@@ -18,18 +18,19 @@ send.addEventListener('click', () => {
       name: nick, message: piii.filter(input.value), time: new Date()
     });
     input.value = '';
+    socket.emit('stop-typing');
   }
 });
 
 socket.on('chat', (chat) => {
 
   if (chat.name == nick) {
-    $("#chat").append(`<li class="message right">
+    $("#chat").append(`<li id="${chat._id}" class="message right">
       <author><b>${chat.name}</b> - ${moment(chat.time).locale('pt-br').calendar()}</author>
       <p>${chat.message}</p></li>`);
   } else {
     audio.play();
-    $("#chat").append(`<li class="message left">
+    $("#chat").append(`<li id="${chat._id}" class="message left">
       <author><b>${chat.name}</b> - ${moment(chat.time).locale('pt-br').calendar()}</author>
       <p>${chat.message}</p></li>`);
   }
@@ -39,17 +40,40 @@ socket.on('chat', (chat) => {
 });
 
 socket.on('previous-chat', (message) => {
-  message.forEach((chat) => {
+   message.forEach((chat) => {
     if (chat.name == nick) {
-      $("#chat").append(`<li class="message right">
+      $("#chat").append(`<li id="${chat._id}" class="message right">
         <author><b>${chat.name}</b> - ${moment(chat.time).locale('pt-br').calendar()}</author>
-        <p>${chat.message}</p></li>`);
+        <p>${chat.message}</p>
+      </li>`);
     } else {
-      $("#chat").append(`<li class="message left">
+      $("#chat").append(`<li id="${chat._id}" class="message left">
         <author><b>${chat.name}</b> - ${moment(chat.time).locale('pt-br').calendar()}</author>
         <p>${chat.message}</p></li>`);
     }
   });
 
   $('.message-list').scrollTop($('.message-list')[0].scrollHeight);
+});
+
+input.addEventListener('input', () => {
+  if (input.value) {
+    socket.emit('typing', nick);
+  } else {
+    socket.emit('stop-typing');
+  }
+});
+
+socket.on('typing', (typing) => {
+  if (typing.length === 1 && typing[0].nick != nick) {
+    $('#typing').html(`💡 ${typing[0].nick} está digitando...`);
+  } else if (typing.length > 1) {
+    $('#typing').html('💡 Várias pessoas estão digitando...');
+  } else {
+    $('#typing').html(' ');
+  }
+});
+
+socket.on('stop-typing', () => {
+  $('#typing').html('');
 });
