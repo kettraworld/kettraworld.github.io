6e480e834f948fa602fd4dbd4be40036116665f7
sebastianjnuwu <sebastianjnuwu@gmail.com>
2023-07-21 14:11:42 -0300

    New version

--- docs/src/js/payment.js
+++
@@ -1,9 +0,0 @@
-
-const query = new URLSearchParams(window.location.search);
-const product = parseInt(query.get('product'));
-
-if (isNaN(product) || product < 1 || product > 5) {
-  window.location.href = 'loja.html';
-}
-
-$("#id").val(query.get('product'));
