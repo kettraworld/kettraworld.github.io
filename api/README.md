65fc0296f273166db0ea1712de028626f6f94f2c
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-07-24 11:49:01 -0300

    v0.0.0
    Signed-off-by: SEBASTIAN JN <sebastianjnuwu@gmail.com>

--- api/README.md
+++
@@ -1,8 +0,0 @@
-
-```bash
-npm install
-```
-
-```bash
-npm run start
-```
--- api/src/api/mercadopago.js
+++
@@ -1,9 +0,0 @@
-import { MercadoPagoConfig, Payment } from 'mercadopago';
-
-const client = new MercadoPagoConfig({
-  accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN 
-});
-
-const mercadopago = new Payment(client);
-
-export { mercadopago };
