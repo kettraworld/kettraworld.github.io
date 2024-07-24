ba219b3229eb439062af529f6b7f3fe1a9b54967
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-07-24 11:48:36 -0300

    v0.0.0
    Signed-off-by: SEBASTIAN JN <sebastianjnuwu@gmail.com>

--- site/src/js/config/axios.js
+++
@@ -1,8 +0,0 @@
-  // Axios configuration for the server 
-const api = axios.create({
-  baseURL: 'http://0.0.0.0:8080/',
-  timeout: 1000,
-  headers: {
-   'authorization': Cookies.get('JWT_TOKEN')
-  }
-});
