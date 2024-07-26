ccb162099d49d1711889ce9aa6e9da67ebb028f4
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-07-26 18:10:49 -0300

    Delete website directory
    Signed-off-by: SEBASTIAN JN <sebastianjnuwu@gmail.com>

--- website/worker.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener("install", event => {
-   console.log("Service worker installed");
-});
-
-self.addEventListener("activate", event => {
-   console.log("Service worker activated");
-});
